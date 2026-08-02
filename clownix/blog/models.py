73aadaf6cc59b171cb99fb24f55b650c5c42d21c
sergey-kolesnik <00volchenok0013@mail.ru>
2026-08-02 15:22:41 +0300

    feat(model): create model post

--- clownix/blog/models.py
+++ clownix/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    """
+    Модель постов для блога
+    """
+
+    STATUS_OPTIONS = (
+        ("published", "Опубликованно"),
+        ("draft", "Черновик"),
+    )
+
+    title = models.CharField(verbose_name="НАзвание поста", max_length=255)
+    slug = models.SlugField(verbose_name="URL", max_length=255, blank=True)
+    description = models.TextField(verbose_name="Краткое описание", max_length=500)
+    text = models.TextField(verbose_name="Содержание поста")
