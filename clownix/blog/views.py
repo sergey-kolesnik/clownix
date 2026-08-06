c5ff524771b8254aa9bda9a7134d1b01fc5bd382
sergey-kolesnik <00volchenok0013@mail.ru>
2026-08-06 15:29:49 +0300

    create template css index

--- clownix/blog/views.py
+++ clownix/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
 
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "index.html")
