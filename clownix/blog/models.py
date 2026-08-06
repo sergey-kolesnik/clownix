a535be502b7e6e1033c476bff4a3f4a3c1d4e6a4
sergey-kolesnik <00volchenok0013@mail.ru>
2026-08-06 14:02:49 +0300

    create category model

--- clownix/blog/models.py
+++ clownix/blog/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Category(models.Model):
+    """
+    Модель категория поста
+    """
+    pass
+
 class Post(models.Model):
     """
     Модель постов для блога
@@ -10,7 +16,7 @@ class Post(models.Model):
         ("draft", "Черновик"),
     )
 
-    title = models.CharField(verbose_name="НАзвание поста", max_length=255)
+    title = models.CharField(verbose_name="Название поста", max_length=255)
     slug = models.SlugField(verbose_name="URL", max_length=255, blank=True)
     description = models.TextField(verbose_name="Краткое описание", max_length=500)
     text = models.TextField(verbose_name="Содержание поста")
